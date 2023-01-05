5dcbe3e016d8861309024acb8e0e65aa7c4fc47a
unknown <HillaryKipkoech>
2023-01-05 03:39:23 -0800

    changes on scroll;  button

--- js/script.js
+++ js/script.js
@@ -38,3 +38,25 @@ const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
 })
+
+// scroll button
+
+// Get the button:
+let mybutton = document.getElementById("upbtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
