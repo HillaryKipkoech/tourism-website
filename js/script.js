90ed34ded40d61193fb31879a209c0d03b859790
hillarykipkoech <kipkoechhillary85@gmail.com>
2023-07-05 11:22:18 +0300

    added contributions

--- js/script.js
+++ js/script.js
@@ -41,10 +41,8 @@ checkbox.addEventListener('change', ()=>{
 
 // scroll button
 
-// Get the button:
 let mybutton = document.getElementById("upbtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -54,9 +52,7 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
 }
